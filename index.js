ff0d31fde5c560005a427d15473f6e8a4ce2d28c
JakubMarkiewicz <markiewicz.jakub2@gmail.com>
2019-11-08 20:50:34 +0100

    whiteList fix

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ module.exports = exports = (secret, whitelist, config = {}) => fn => {
             ? whitelist.find(v => (Array.isArray(v) ? v[0] === pathname : v === pathname)) || []
             : []
         const pathConfig = Array.isArray(whiteList[0]) ? whiteList[0][1] : {}
-        const method = pathConfig.method ? pathConfig.method === req.method : false
-        if (!bearerToken && (!whiteList || !method)) {
+        const method = pathConfig.method ? pathConfig.method === req.method : true
+        if (!bearerToken && (!whiteList[0] || !method)) {
             res.writeHead(401)
             res.end(config.resAuthMissing || 'missing Authorization header')
             return
@@ -30,7 +30,7 @@ module.exports = exports = (secret, whitelist, config = {}) => fn => {
             const token = bearerToken.replace('Bearer ', '')
             req.jwt = jwt.verify(token, secret)
         } catch (err) {
-            if (!whiteList || !method) {
+            if (!whiteList[0] || !method) {
                 res.writeHead(401)
                 res.end(config.resAuthInvalid || 'invalid token in Authorization header')
                 return
